dc719274798bbb537bd56f56376e41c0784e1125
Simple Calculator kaofang EiEi <pasukree_khodnarin@hotmail.com>
2017-11-02 16:22:25 +0700

    finish!!!

--- alpaca-web/src/components/Destination.js
+++ alpaca-web/src/components/Destination.js
@@ -9,14 +9,56 @@ import '../css/destination.css'
 const Destination = () => {
 
     return (
-        <div>
+        <div className="f">
             {/* container */}
             <p className="t-color font-opan font-size-cyd text-center">CHOOSE YOUR DESTINATION</p>
             <hr width="20%" size="100%" color="orange"></hr>
-            <div className="container b-image">
-                {<p className="bottomright">CULTURAL</p>}
+            <div className="container-fluid">
+                <div className="row b-image">
+                    <div className="col-6 col-md-8">
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <p className="f-topic font-opan">Cultural</p>
+                        <a href="" className="choose-button font-opan">
+                            CHOOSE THIS
+                        </a>
+                    </div>
+                </div>
+
+                <div className="row b-image2">
+                    <div className="col-6 col-md-8">
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <p className="f-topic font-opan">Historical</p>
+                        <a href="" className="choose-button">
+                            CHOOSE THIS
+                        </a>
+                    </div>
+                </div>
+
+                <div className="row b-image3">
+                    <div className="col-6 col-md-8">
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <p className="f-topic font-opan">Lifestyle</p>
+                        <a href="" className="choose-button">
+                            CHOOSE THIS
+                        </a>
+                    </div>
+                </div>
+
+                <div className="row b-image4">
+                    <div className="col-6 col-md-8">
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <p className="f-topic font-opan">Natural</p>
+                        <a href="" className="choose-button">
+                            CHOOSE THIS
+                        </a>
+                    </div>
+                </div>
+                
             </div>
-            
             {/* <img src={pic1} width="100%" className="center"/>
             <img src={pic2} width="100%" className="center"/>
             <img src={pic3} width="100%" className="center"/>
